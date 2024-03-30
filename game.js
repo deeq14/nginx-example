c7dcdee58e4a1d60912e46cde376a71f9043e127
deeq14 <deeq14@gmail.com>
2024-03-30 23:28:32 +0800

    Update and rename game2.js to game.js

---
+++ game.js
@@ -0,0 +1,41 @@
+// Example usage:
+const cssCode = `p {
+  width: 100px;
+  height: 100px;
+  background: red;
+  position: relative;
+  animation: mymove 5s infinite;
+}`;
+
+function splitCssRules(cssCode) {
+  // Remove newline characters from the CSS code
+  const cssM = cssCode.replace(/\n/g, '');
+
+  let cssArray = [];
+  let startIndex = 0;
+  let nestedBrackets = 0;
+
+  for (let i = 0; i < cssM.length; i++) {
+    if (cssM[i] === '{') {
+      nestedBrackets++;
+    } else if (cssM[i] === '}') {
+      nestedBrackets--;
+    }
+
+    if (nestedBrackets === 0 && cssM[i] === '}') {
+      cssArray.push(cssM.substring(startIndex, i + 1).trim());
+      startIndex = i + 1;
+    }
+  }
+
+  return cssArray;
+}
+
+const cssArray = splitCssRules(cssCode);
+
+const styleSheet = document.styleSheets[0];
+cssArray.forEach(function (rule) {
+  styleSheet.insertRule(rule, styleSheet.cssRules.length);
+});
+
+console.log(cssArray);
